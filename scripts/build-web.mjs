e35bfcba05ee0c2a5c53132281457c18e0dcdbb6
ppatrickxxz-dev <ppatrickxxz@gmail.com>
2026-09-14 19:57:26 -0300

    perf: compact safe css statement newlines

--- scripts/build-web.mjs
+++ scripts/build-web.mjs
@@ -85,7 +85,8 @@ async function compactCssLayout(directory) {
       .replace(/\/\*[\s\S]*?\*\//g, '')
       .replace(/\n[ \t]+/g, '\n')
       .replace(/[ \t]+\n/g, '\n')
-      .replace(/\n{2,}/g, '\n');
+      .replace(/\n{2,}/g, '\n')
+      .replace(/([;{}])\n/g, '$1');
     if (compacted !== source) await writeFile(path, compacted, 'utf8');
   }
 }
